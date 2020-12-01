79565721507d909b2fba7b6b65cb1e6b7977a1c9
Park, Jiwoo <parkjiwoo9980@gmail.com>
2020-12-02 02:15:40 +0900

    코드정리-usercontroller주석삭제

--- src/main/java/net/skhu/controller/UserController.java
+++ src/main/java/net/skhu/controller/UserController.java
@@ -26,11 +26,6 @@ public class UserController {
 	public String signin() {
 		return "board/signin.html";
 	}
-	/*
-	 * @PostMapping("/signin")
-	 * public String singin(UserDto userDto) {
-	 * userService.savePost(userDto); return "redirect:/"; }
-	 */
 
 	@PostMapping("/signin")
 	public String singin(Model model, UserDto userDto, HttpSession session) {
